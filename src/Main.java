75d873373db0834a1dcee10e169bd7f8d04a8e45
axel Name <gandullaaxel727.gmail.com>
2022-07-06 17:21:17 -0300

    Numeros primos: problem1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        new Scanner(System.in);
+        System.out.println("Ingrese el numero");
+        Scanner Num1 = new Scanner(System.in);
+        int i = Num1.nextInt();
+        int contador = 0;
+        System.out.println("Los divisores de: "+i+" son:");
+        for (int j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                System.out.print(j+ " , ");
+                contador++;
+            }
+        }
+            if (contador <= 2) {
+                System.out.println("\nEl numero "+ i + " es primo  ");
+            }else if (contador > 2){
+                System.out.println("\nEl numero es compuesto");
+            }
+
+        }
+   }
