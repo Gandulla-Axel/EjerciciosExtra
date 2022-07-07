0d82df19eb75366ef111c9771a912a1249806209
axel Name <gandullaaxel727.gmail.com>
2022-07-07 12:35:18 -0300

    Numeros primos comprendidos, problem2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Scanner(System.in);
+        System.out.println("Ingrese el numero");
+        Scanner Num1 = new Scanner(System.in);
+        int numero = Num1.nextInt();
+        int contador = 0;
+        System.out.println("Los numeros primos comprendidos entre: 1 y"+numero+"  son: ");
+
+        for (int i = 1; i <= numero; i++) {
+
+        for (int j = 1; j <=i; j++) {
+            if (i % j == 0) {
+              //  System.out.print(j+ " , ");
+                contador++;
+            }
+        }
+
+        if (contador <= 2 && contador!=1) {
+              System.out.println(i+" ,");
+        }
+        contador=0;
+    }
     }
 }
